e1d4702301bb280f4cb83faa3264efcfd9ceb60f
Joe Finkel <joefin@67-194-127-53.wireless.umnet.umich.edu>
2013-10-06 21:07:36 -0400

    Trade off

--- WeWrite/ASIAppDelegate.h
+++ WeWrite/ASIAppDelegate.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Collabrify/Collabrify.h>
 #import "LimitedTextView.h"
 
 @interface ASIAppDelegate : UIResponder <UIApplicationDelegate>
--- WeWrite/ASIViewController.m
+++ WeWrite/ASIViewController.m
@@ -7,17 +7,38 @@
 //
 
 #import "ASIViewController.h"
+#import <Collabrify/Collabrify.h>
 
-@interface ASIViewController ()
+@interface ASIViewController () < CollabrifyClientDelegate, CollabrifyClientDataSource>
+
+@property (strong, nonatomic) CollabrifyClient *client;
+
 
 @end
 
 @implementation ASIViewController
 
+- (void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    if (![self client])
+    {
+        //From how to integrate collabrify document
+        NSError *error;
+        //[self setClient:[[CollabrifyClient alloc] initWithGmail:@"usersGmail@gmail.com"
+        //                                            displayName:@"User's Display Name"
+        //                                           accountGmail:@"441fall2013@umich.edu"
+        //                                            accessToken:@"XY3721425NoScOpE"
+        //                                         getLatestEvent:NO
+        //                                                  error:&error]];
+        [[self client] setDelegate:self];
+        [[self client] setDataSource:self];
+    }
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
