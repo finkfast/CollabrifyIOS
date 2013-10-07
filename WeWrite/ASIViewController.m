f2ad5b5c83a1bf8b1dcc31b02447a235a4dd5ced
Joe Finkel <joefin@67-194-127-53.wireless.umnet.umich.edu>
2013-10-06 22:14:10 -0400

    Finally collabrifying

--- WeWrite/ASIViewController.m
+++ WeWrite/ASIViewController.m
@@ -25,12 +25,12 @@
     {
         //From how to integrate collabrify document
         NSError *error;
-        //[self setClient:[[CollabrifyClient alloc] initWithGmail:@"usersGmail@gmail.com"
-        //                                            displayName:@"User's Display Name"
-        //                                           accountGmail:@"441fall2013@umich.edu"
-        //                                            accessToken:@"XY3721425NoScOpE"
-        //                                         getLatestEvent:NO
-        //                                                  error:&error]];
+        [self setClient:[[CollabrifyClient alloc] initWithGmail:@"usersGmail@gmail.com"
+                                                    displayName:@"User's Display Name"
+                                                   accountGmail:@"441fall2013@umich.edu"
+                                                    accessToken:@"XY3721425NoScOpE"
+                                                 getLatestEvent:NO
+                                                          error:&error]];
         [[self client] setDelegate:self];
         [[self client] setDataSource:self];
     }
